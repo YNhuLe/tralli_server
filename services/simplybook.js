a722961d24aff893e01e0a8ddd7e5c7d358e632b
ynhule96 <Lenhuy10011996@gmail.com>
2025-08-07 23:53:11 -0400

    fix the access denied error for simplybook.me

--- services/simplybook.js
+++ services/simplybook.js
@@ -1,39 +1,56 @@
 import axios from "axios";
 import "dotenv/config";
 
-const BASE_URL = "https://user-api.simplybook.me/admin";
-const getServices = async (req,res) =>{
+// const BASE_URL = "https://user-api.simplybook.me";
+// const BASE_URL = `https://user-api.simplybook.me`;
+const BASE_URL = `https://user-api.simplybook.me`;
+
+
+console.log("Loaded company login:", process.env.SIMPLYBOOK_COMPANY_LOGIN);
+console.log("Loaded API key:", process.env.SIMPLYBOOK_API_KEY);
+console.log("Using company login:", process.env.SIMPLYBOOK_COMPANY_LOGIN);
+
+const getServices = async () =>{
     try{
-const authResponse = await axios.post(`${BASE_URL}/login`, {
 
-    
-  "jsonrpc": "2.0",
-  "method": "getToken",
-  "params": {
-    "company": "booktralli",
-    "api_key": "c363bd4ae1a5227ccb472dd9891afe4b92d615d3b198d023d3244396942a65c4"
-  },
-  "id": 1
-
-
-    //apiKey: process.env.SIMPLYBOOK_API_KEY,
-});
-const token = authResponse.data.result;
-const serviceResponse = await axios.post(BASE_URL, {
+const authResponse = await axios.post(`${BASE_URL}/admin`, {
     jsonrpc: "2.0",
     method: 'getToken',
         params: {
-        api_key: process.env.SIMPLYBOOK_API_KEY,
+
+companyLogin: process.env.SIMPLYBOOK_COMPANY_LOGIN,
+        apiKey:process.env.SIMPLYBOOK_API_KEY,
+   
       },
     id: 1,
+});
+const token = authResponse.data.result;
+
+if(!token){
+    console.log("failed to get the token: ", authResponse.data);
+    return
+    
+}
+//use token to fetch services
+const serviceResponse = await axios.post(`${BASE_URL}/user`, {
+    jsonrpc: "2.0",
+    method: 'getServiceList',
+        params: {},
+    id: 2,
 }, {
     headers: {
-        'X-Company-Login': process.env.SIMPLYBOOK_COMPANY_LOGIN,
-        'X-Token': token
+     "X-Company-Login": process.env.SIMPLYBOOK_COMPANY_LOGIN,
+     "X-Token": token
     }}
     );
+console.log("Service response: ", serviceResponse.data);
+console.log("Token: ", token);
+
+    // res.json(serviceResponse.data.result)
     } catch(error){
-        console.log("Error fetching services: ", error);
-        
+        console.log("Error fetching services: ", error.response?.data || error.message);
+        // res.status(500).json({ error: "Failed to fetch services" });
     }
 }
+
+getServices()
